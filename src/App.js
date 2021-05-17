e4755d2d51399c1b878b1ce4c9fce849597710fc
Jaime Salgado <jsalg10@gmail.com>
2021-05-17 10:42:11 -0500

    created task components, added react icons, added reminder toggle ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Gym",
+        day: 'Feb 4th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "run",
+        day: 'Feb 6th at 2:30PM',
+        reminder: true,
+    },
+])
+
+//delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !==id))
+}
+
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=> task.id ===id? {...task, reminder: !task.reminder}: task))
+}
   return (
     <div className="container">
-      <Header/>      
+      <Header />   
+      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks to show" }
     </div>
   );
 }
